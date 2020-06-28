f498257295c8809e53bb1063e62791f94e203f53
Fhuu <dirobaloska@outlook.com>
2020-06-28 21:19:19 +0200

    initial commit

---
+++ react-project/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//By using import we can link css into react js file
+import './index.css';
+
+/*
+  This is with class
+  //In this case, this is the class that being rendered.
+  class Test extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Hello</h1>
+          <span id="hell-yeah">This is my first react application</span>
+        </div>
+        
+      );
+    };
+  }
+
+  //This is to render the class that being passed to.
+  //To render we need ReactDOM.render();
+  ReactDOM.render(
+    //The class to be rendered
+    <Test />, 
+    //The position of rendering
+    document.getElementById("root")
+  );
+*/
